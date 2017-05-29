fa7e033f4fb42ec02cc14cf5f4733734fe6bf01d
Pruxis <llavaert@gmail.com>
2017-05-29 17:53:27 +0200

    Installing dependencies, creating routes and standard components

---
+++ src/components/NotFound.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NotFound = ({ location }) => {
+  return <h1>Page {location.pathname} was not found.</h1>;
+};
+
+export default NotFound;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import Login from "./components/Login";
+import NotFound from "./components/NotFound";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import registerServiceWorker from "./registerServiceWorker";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
+
+const Root = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={App} />
+        <Route path="/admin" component={Login} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  );
+};
+
+ReactDOM.render(<Root />, document.getElementById("root"));
 registerServiceWorker();
