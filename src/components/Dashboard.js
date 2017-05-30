a2bc5309ed4f367aa461fe8d9f8c3a6ab521769a
Pruxis <llavaert@gmail.com>
2017-05-30 18:12:48 +0200

    Importing components in Dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,20 @@
 import React, { Component } from "react";
 
 import NavbarHeader from "./navigation/NavbarHeader";
+import SectionHeader from "./navigation/SectionHeader";
+import Sidebar from "./navigation/Sidebar";
+import UserMenu from "./navigation/UserMenu";
+import NavigationLink from "./navigation/NavigationLink";
+
+import image from "../assets/images/logo_icon_dark.png";
+import logo from "../assets/images/logo_light.png";
 
 /* Importing theme styles */
 import "../assets/css/icons/icomoon/styles.css";
-import "../assets/css/bootstrap.css";
-import "../assets/css/core.css";
-import "../assets/css/components.css";
-import "../assets/css/colors.css";
+import "../assets/css/bootstrap.min.css";
+import "../assets/css/core.min.css";
+import "../assets/css/components.min.css";
+import "../assets/css/colors.min.css";
 
 /* Dumb wrapper component that wraps the content inside a page-container*/
 const Container = ({ children }) => {
@@ -22,7 +29,41 @@ const Container = ({ children }) => {
 
 class Dashboard extends Component {
   render() {
-    return <NavbarHeader />;
+    return (
+      <div>
+        <NavbarHeader logo={image} logoAlt="logo" logoLink="/" />
+        <Container>
+          <Sidebar
+            userMenu={
+              <UserMenu
+                profile={{
+                  image: image,
+                  name: "React CMS",
+                  location: "Somewhere, BE"
+                }}
+              />
+            }
+          >
+            <SectionHeader title="Main" />
+            <NavigationLink title="Dashboard" to="/" icon="icon-home4" />
+            <NavigationLink title="Pages" to="/pages" icon="icon-copy" />
+          </Sidebar>
+          <div className="content-wrapper">
+            <div className="content">
+              <div className="row">
+                <div className="col-lg-7">
+                  <div className="panel panel-flat">
+                    <div className="panel-heading">
+                      <h6 className="panel-title">Test</h6>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Container>
+      </div>
+    );
   }
 }
 
