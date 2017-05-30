51ecf6d9938c453e4da540b75a313d8e6e4ae675
Pruxis <llavaert@gmail.com>
2017-05-30 15:27:53 +0200

    Creating NavbarHeader component

---
+++ src/components/navigation/NavbarHeader.js
@@ -0,0 +1,41 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+/**
+ * Author: Laurens Lavaert
+ * Date: 30/05/2017
+ * @param logoLink: Link that should be coupled behind the clickable logo. Pressing this will redirect you to that url.
+ *  --> Standard reference is to the "/" page, gets overwritten if property is provided.
+ * @param logo: Source of the logo image.
+ * @param logoAlt: Alternative text that belongs to the logo.
+ * @param children: Mobile Navigation Component goes in here to provide navigation on mobile devices.
+ */
+
+const NavbarHeader = ({ logoLink, logo, logoAlt, children, ...props }) => {
+  return (
+    <div className="navbar navbar-default header-highlight" {...props}>
+      <div className="navbar-header">
+        <Link to={logoLink || "/"} className="navbar-brand">
+          <img src={logo} alt={logoAlt} />
+        </Link>
+        <ul className="nav navbar-nav visible-xs-block">
+          <li>
+            <a className="sidebar-mobile-main-toggle">
+              <i className="icon-paragraph-justify3" />
+            </a>
+          </li>
+        </ul>
+      </div>
+      {children}
+    </div>
+  );
+};
+
+export default NavbarHeader;
+
+NavbarHeader.propTypes = {
+  logoLink: PropTypes.string,
+  logo: PropTypes.string.isRequired,
+  logoAlt: PropTypes.string.isRequired
+};
