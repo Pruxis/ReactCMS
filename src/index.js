190758c530166327fa7a254094e51d7fe6f0e720
Pruxis <llavaert@gmail.com>
2017-05-30 16:03:30 +0200

    Delegate styles to the component itself instead of root

--- src/index.js
+++ src/index.js
@@ -1,23 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
+import Dashboard from "./components/Dashboard";
 import Login from "./components/Login";
 import NotFound from "./components/NotFound";
 
 import registerServiceWorker from "./registerServiceWorker";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import "./assets/css/icons/icomoon/styles.css";
-import "./assets/css/bootstrap.css";
-import "./assets/css/core.css";
-import "./assets/css/components.css";
-import "./assets/css/colors.css";
-
 const Root = () => {
   return (
     <Router>
       <Switch>
-        <Route path="/" exact component={App} />
+        <Route path="/" exact component={Dashboard} />
         <Route path="/admin" component={Login} />
         <Route component={NotFound} />
       </Switch>
