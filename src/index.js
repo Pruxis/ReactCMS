68de0f16ed8736ef72686d065f96598a78cecac1
Pruxis <llavaert@gmail.com>
2017-05-30 13:56:37 +0200

    Importing Limitless template

--- src/index.js
+++ src/index.js
@@ -5,12 +5,13 @@ import Login from "./components/Login";
 import NotFound from "./components/NotFound";
 
 import registerServiceWorker from "./registerServiceWorker";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import "./assets/css/icons/icomoon/styles.css";
+import "./assets/css/bootstrap.css";
+import "./assets/css/core.css";
+import "./assets/css/components.css";
+import "./assets/css/colors.css";
 
 const Root = () => {
   return (
